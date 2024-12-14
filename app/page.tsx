bae672e149d8d7b97f9725e8608b8b59e14abada
Otohiko Mori <ottumori8940@gmail.com>
2024-12-14 18:11:41 +0900

    Bookの疑似データの出力

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,70 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
+"use client";
+
+import Book from "./components/Book";
+
+// 疑似データ
+const books = [
+  {
+    id: 1,
+    title: "Book 1",
+    thumbnail: "/thumbnails/discord-clone-udemy.png",
+    price: 2980,
+    author: {
+      id: 1,
+      name: "Author 1",
+      description: "Author 1 description",
+      profile_icon: "https://source.unsplash.com/random/2",
+    },
+    content: "Content 1",
+    created_at: new Date().toString(),
+    updated_at: new Date().toString(),
+  },
+  {
+    id: 2,
+    title: "Book 2",
+    thumbnail: "/thumbnails/notion-udemy.png",
+    price: 1980,
+    author: {
+      id: 2,
+      name: "Author 2",
+      description: "Author 2 description",
+      profile_icon: "https://source.unsplash.com/random/3",
+    },
+    content: "Content 2",
+    created_at: new Date().toString(),
+    updated_at: new Date().toString(),
+  },
+  {
+    id: 3,
+    title: "Book 3",
+    price: 4980,
+    thumbnail: "/thumbnails/openai-chatapplication-udem.png",
+    author: {
+      id: 3,
+      name: "Author 3",
+      description: "Author 3 description",
+      profile_icon: "https://source.unsplash.com/random/4",
+    },
+    content: "Content 3",
+    created_at: new Date().toString(),
+    updated_at: new Date().toString(),
+  },
+  // 他の本のデータ...
+];
+
+// eslint-disable-next-line @next/next/no-async-client-component
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+    <>
+      <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
+        <h2 className="text-center w-full font-bold text-3xl mb-2">
+          Book Commerce
+        </h2>
+        {books.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }

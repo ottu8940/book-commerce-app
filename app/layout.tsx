870ab458981507dba61106f79482c6ff51404493
Otohiko Mori <ottumori8940@gmail.com>
2024-12-15 15:00:58 +0900

    ユーザー情報の取得

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { NextAuthProvider } from "./lib/next-auth/provider";
 
 const geistSans = Noto_Sans_JP({
   subsets: ["latin"],
@@ -27,8 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
-        {children}
+        <NextAuthProvider>
+          <Header />
+          {children}
+        </NextAuthProvider>
       </body>
     </html>
   );
